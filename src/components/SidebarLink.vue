610be5ad909f52728e89e337ab14ebdc5a61537d
IshiniAvindya <ishiniavindya2000@gmail.com>
2023-07-31 18:40:02 +0530

    chore:begin implement login

---
+++ src/components/SidebarLink.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import { toRefs } from 'vue'
+import { RouterLink } from 'vue-router'
+
+const props = defineProps<{
+  to: string
+  text: string
+  isSidebarOpen: boolean
+}>()
+const { to, text, isSidebarOpen } = toRefs(props)
+</script>
+
+<template>
+  
+<RouterLink :to="to" class="flex items-center gap-4 m-6 p-1.5"> 
+<slot name="icon"></slot>
+{{ isSidebarOpen ? text : "" }}
+</RouterLink>
+ 
+ 
+
+</template>
+
+
+<style lang="scss" scoped>
+a{
+    transition: all 200ms;
+    border-radius: 8px;
+
+    &:hover{
+    background-color: var(--n-color-primary-200-transparent);
+  }
+
+    &:active{
+      background-color: var(--n-color-primary-600-transparent);
+    }
+  }
+</style>
