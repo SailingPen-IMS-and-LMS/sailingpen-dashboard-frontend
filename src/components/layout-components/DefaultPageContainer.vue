f17e1d90c495560adb080ba485d7b763a6e7eafc
Nethsara Sandeepa Elvitigala <nethsarasandeepaelvitigala@gmail.com>
2023-08-06 23:17:37 +0530

    add demo pages for the blog

--- src/components/layout-components/DefaultPageContainer.vue
+++ src/components/layout-components/DefaultPageContainer.vue
@@ -3,3 +3,16 @@
     <RouterView />
   </main>
 </template>
+
+<style lang="scss">
+.default-page-container {
+  padding-inline: 1rem;
+  margin: 0 auto;
+  width: 100%;
+  margin-top: 2rem;
+
+  @include mq(lg) {
+    padding-inline: 2rem;
+  }
+}
+</style>
